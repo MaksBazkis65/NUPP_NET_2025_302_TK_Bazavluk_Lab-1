0137d8f6a22cbffc0cdd1809ee897bf56e6b87a3
MaksBazkis65 <128786014+MaksBazkis65@users.noreply.github.com>
2025-06-06 07:08:48 +0300

    Add files via upload

---
+++ NUPP_NET_2025_302_TK_BazavlukMaksym_Lab2/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Lab2!");
+        }
+    }
+}
